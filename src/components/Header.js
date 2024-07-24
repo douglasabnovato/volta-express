b6a45a459f4198ad31dee6dce017481e3776f080
douglasabnovato <douglasabnovato@gmail.com>
2024-07-24 14:37:19 -0300

    adicionado devDependencies para corrigir biblioteca

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,6 @@ import PropTypes from "prop-types";
 import Toolbar from "@mui/material/Toolbar";
 import Button from "@mui/material/Button";
 import IconButton from "@mui/material/IconButton";
-import Typography from "@mui/material/Typography";
 import AppBar from "@mui/material/AppBar";
 import Container from "@mui/material/Container";
 import Box from "@mui/material/Box";
